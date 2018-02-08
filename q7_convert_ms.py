74ee1b772dfadf7d4362af296e09683564416e68
Nicky Ren <ren.nicky@dhs.sg>
2018-02-08 22:25:12 +0800

    Update q7_convert_ms.py

--- q7_convert_ms.py
+++ q7_convert_ms.py
@@ -1,14 +1,15 @@
 # Converting milliseconds to hours, minutes, and seconds
 
+n = int(input("Enter milliseconds: "))
+
 def convert_ms(n):
-    # 1h = 60 mins = 3600 sec = 3600 * 1000 ms
-    secConverted = int(mSec/1000)       
-    time = '{0}:{1}:{2}'.format(int(secConverted/3600), int((secConverted%3600)/60), secConverted%60 )
-    
-    return _time
+    rawSecond = n // 1000
+    second = rawSecond % 60
+    rawMinute = rawSecond // 60
+    minute = rawMinute % 60
+    hour = rawMinute // 60
+    print("{0}:{1}:{2}".format(hour,minute,second))
 
-n = int(input())
-print(convert_ms(n))
-
+convert_ms(n)
 
 
